2d83fc421c970617fb99ff5fd0d31fb420eee8e3
Sathyanarayanan Raghunathan <M1046085@mindtree.com>
2018-09-12 00:44:45 +0530

    DB Changes

---
+++ DBScripts/Testscript1.sql
@@ -0,0 +1,13 @@
+# Deployment Scripts
+CREATE DATABASE menagerie;
+
+USE menagerie
+
+CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20),
+species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+
+
+
+
+# Rollback Scripts
+
